d45d8dab9ba45989617b2a2880935a34553d776b
Bat Sheva Winter <BAT24327@gmail.com>
2026-07-23 22:07:49 +0300

    Delete js directory

--- js/Instructions.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("startBtn").addEventListener("click", function () {
-  const difficulty = document.getElementById("difficulty").value;
-  window.location.href = `game.html?difficulty=${difficulty}`;
-});
